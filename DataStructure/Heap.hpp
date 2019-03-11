83ebfc352f143bc84a7130b482e353707cfe3290
Monster <838009062@qq.com>
2019-03-12 00:14:52 +0800

    开始堆的代码

---
+++ DataStructure/Heap.hpp
@@ -0,0 +1,15 @@
+#ifndef _HEAP_HPP_
+#define _HEAP_HPP_
+
+#include <iostream>
+
+class Heap
+{
+	Heap() {}
+	Heap(const std::vector<int>& arr);
+	~Heap();
+	MakeHeap(const std::vector<int>& arr);
+
+};
+
+#endif
